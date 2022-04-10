2006e41967e2905815c181e42246897980e576be
itsmattpaw <itsmattpaw@gmail.com>
2022-04-10 17:04:38 -0400

    commit tree 01cbbc02150a78e6ea40600a511dbc8fdb1e9141

---
+++ app/application.rb
@@ -0,0 +1,8 @@
+class Application
+    def call(env)
+        return [ 200, {'Content-Type' => 'text/html'}, response]
+    end
+    def response
+        Time.now.hour < 12 ? ["Good Morning!"] : ["Good Afternoon!"]
+    end
+end
